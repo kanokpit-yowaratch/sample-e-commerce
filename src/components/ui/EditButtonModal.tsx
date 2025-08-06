ca599ee3c8a123d2737b3fcacc733cadadd25d5f
Kanokpit <thana.drnn@hotmail.com>
2025-08-07 01:56:45 +0700

    fix: review code

--- src/components/ui/EditButtonModal.tsx
+++ src/components/ui/EditButtonModal.tsx
@@ -3,9 +3,11 @@
 import React from 'react';
 import { hasPermission } from '@/lib/permission';
 import { PermissionElementProps } from '@/types/permission';
+import { useModal } from '@/stores/zustand/modalStore';
 
 function EditButtonModal({ resource, action, icon: Icon, id, className }: Readonly<PermissionElementProps>) {
   const isAllowed = hasPermission(resource, action);
+  const { open } = useModal();
 
   if (!isAllowed) {
     return null;
--- src/lib/schemas/login-schema.ts
+++ src/lib/schemas/login-schema.ts
@@ -3,13 +3,10 @@ import { z } from 'zod';
 export const loginSchema = z.object({
 	email: z
 		.string()
-		.email('Invalid email address')
-		.min(9, 'Minimum 9 characters') // abc@de.fg
 		.max(125, 'Maximum 125 characters')
 		.nonempty('Email is required'),
 	password: z
 		.string()
-		.min(8, 'Minimum 8 characters')
 		.max(125, 'Maximum 125 characters')
 		.nonempty('Password is required'),
 });
